e29f67572a5ee48a153e37b3fc3f8d1f8fddbe24
Michael Koprowski <mkoprowski04@gmail.com>
2018-07-25 14:39:00 -0500

    Final Commit

--- RandomNameApp/www/scripts/index.js
+++ RandomNameApp/www/scripts/index.js
@@ -10,21 +10,134 @@
     function onDeviceReady() {
         // Handle the Cordova pause and resume events
         document.addEventListener( 'pause', onPause.bind( this ), false );
-        document.addEventListener( 'resume', onResume.bind( this ), false );
-        
-        // TODO: Cordova has been loaded. Perform any initialization that requires Cordova here.
-        var parentElement = document.getElementById('deviceready');
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+        document.addEventListener('resume', onResume.bind(this), false);
     };
 
-    function onPause() {
-        // TODO: This application has been suspended. Save application state here.
-    };
+    function onPause() {};
 
-    function onResume() {
-        // TODO: This application has been reactivated. Restore application state here.
-    };
-} )();
+    function onResume() {};
+})();
+
+var allNames = [];
+var resultSet = [];
+
+document.getElementById("searchBar").addEventListener("keyup", search);
+document.getElementById("searchBar").addEventListener("search", search);
+document.getElementById("sortOptions").addEventListener("change", sort);
+
+document.getElementById("genNames").addEventListener("click", function () {
+    $.getJSON("http://uinames.com/api/?amount=25", function (data) {
+        allNames = data;
+        sort();
+        displayNames(allNames);
+    });
+});
+
+function displayNames(nameSet) {
+    if (nameSet === null)
+        document.getElementById("nameList").innerHTML = "<p>Please generate some names.</p>";
+    else if (nameSet.length === 0)
+        document.getElementById("nameList").innerHTML = "<p>No matches found.</p>";
+    else {
+        var formatNames = "";
+        for (var i = 0; i < nameSet.length; i++) {
+            formatNames += "<div><p>";
+            formatNames += "<span class='name'>" + nameSet[i].name + " " + nameSet[i].surname +
+                "<br></span>";
+
+            if (nameSet[i].gender === "male")
+                formatNames += "<span class='gender'>Gender: <span class='male'>";
+            else
+                formatNames += "<span class='gender'>Gender: <span class='female'>";
+            formatNames += nameSet[i].gender + "</span></span>";
+
+            formatNames += "<span class='region'> Region: " + nameSet[i].region + "</span>";
+
+            formatNames += "</p></div>";
+        }
+        document.getElementById("nameList").innerHTML = formatNames;
+    }
+}
+
+function search() {
+    resultSet = [];
+    var query = document.getElementById("searchBar").value;
+    query = query.toLowerCase();
+
+    if (query.replace(/\s+/g, '') === "") resultSet = allNames;
+    else {
+        for (var i = 0; i < allNames.length; i++) {
+            if (allNames[i].name.toLowerCase().includes(query) ||
+                allNames[i].surname.toLowerCase().includes(query) ||
+                allNames[i].region.toLowerCase().includes(query)) {
+                resultSet.push(allNames[i]);
+            }
+        }
+    }
+    displayNames(resultSet);
+}
+
+function sort() {
+    var sortBy = document.getElementById("sortOptions").value;
+
+    if (sortBy === "fname") sortByFname();
+    else if (sortBy === "lname") sortByLname();
+    else if (sortBy === "gender") sortByGender();
+    else if (sortBy === "region") sortByRegion();
+}
+
+function sortByFname() {
+    if (allNames !== null && allNames.length > 0) {
+        allNames.sort(function (a, b) {
+            var valA = a.name.toLowerCase();
+            var valB = b.name.toLowerCase();
+
+            if (valA < valB) return -1;
+            if (valA > valB) return 1;
+            return 0;
+        });
+        search();
+    }
+}
+
+function sortByLname() {
+    if (allNames !== null && allNames.length > 0) {
+        allNames.sort(function (a, b) {
+            var valA = a.surname.toLowerCase();
+            var valB = b.surname.toLowerCase();
+
+            if (valA < valB) return -1;
+            if (valA > valB) return 1;
+            return 0;
+        });
+        search();
+    }
+}
+
+function sortByGender() {
+    if (allNames !== null && allNames.length > 0) {
+        allNames.sort(function (a, b) {
+            var valA = a.gender.toLowerCase();
+            var valB = b.gender.toLowerCase();
+
+            if (valA < valB) return -1;
+            if (valA > valB) return 1;
+            return 0;
+        });
+        search();
+    }
+}
+
+function sortByRegion() {
+    if (allNames !== null && allNames.length > 0) {
+        allNames.sort(function (a, b) {
+            var valA = a.region.toLowerCase();
+            var valB = b.region.toLowerCase();
+
+            if (valA < valB) return -1;
+            if (valA > valB) return 1;
+            return 0;
+        });
+        search();
+    }
+}
